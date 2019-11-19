d119ecebf0c0dfe8a89eba4adcf9bb373011ca6c
Thibault Vlieghe <thibault.vlieghe@mcgill.ca>
2019-11-19 11:46:20 -0500

    extended the ttl catching to more diverse possibilties, t T r R, besides 5. Implementation via config file and more modular modification is wishable

--- src/ttl_catch_keyboard.py
+++ src/ttl_catch_keyboard.py
@@ -4,7 +4,11 @@ import keyboard  # using module keyboard
 def wait_for_ttl_keyboard():
     while True:
         try:
-            if keyboard.is_pressed('5'):
+            if keyboard.is_pressed('5') or \
+                    keyboard.is_pressed('T') or \
+                    keyboard.is_pressed('t') or \
+                    keyboard.is_pressed('r') or \
+                    keyboard.is_pressed('R'):
                 print('You Pressed the Key 5!')
                 break  # finishing the loop
             else:
